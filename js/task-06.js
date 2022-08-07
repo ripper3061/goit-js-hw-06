b36bafec94067ea2a491949dbe75d9805155f345
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-07 20:49:15 +0300

    6 task

--- js/task-06.js
+++ js/task-06.js
@@ -1,5 +1,11 @@
-// const validationInput = document.querySelector("input");
+const validationInput = document.querySelector("input");
+const inputValueAmount = document.querySelector('input[data-length="6"]');
 
-// validationInput.addEventListener("blur", () => {
-//   validationInput.id = validationInput.id + ".valid";
-// });
+validationInput.addEventListener("blur", onInputValueChecks);
+
+function onInputValueChecks(event) {
+  event.currentTarget.value.length !== Number(validationInput.dataset.length)
+    ? validationInput.classList.add("invalid")
+    : validationInput.classList.remove("invalid");
+  validationInput.classList.add("valid");
+}
