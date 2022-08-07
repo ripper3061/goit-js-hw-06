1a3b8e9a82c1ba2f00c6302715834306ac8eca1d
Vladimir Ponomarov <ripper3061@gmail.com>
2022-08-07 19:02:35 +0300

    8 Task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formObj = {};
+
+  formObj.email = `${mail}`;
+  formObj.password = `${password}`;
+
+  if (mail === "" || password === "") {
+    return alert("Все поля должны быть заполнены!");
+  }
+
+  console.log(formObj);
+  event.currentTarget.reset();
+}
